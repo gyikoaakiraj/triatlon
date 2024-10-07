42b09d773c5a0da25449f066d1bf18b167b91bf7
gyikoaakiraj <97527727+gyikoaakiraj@users.noreply.github.com>
2024-10-07 12:47:44 +0200

    Add files via upload

---
+++ Dolgozat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"../../src/forras.txt");
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] splitted = line.Split(';');
+                new Versenyzo(splitted[0], splitted[1], int.Parse(splitted[2]), char.Parse(splitted[3]), splitted[4], splitted[5], splitted[6], splitted[7], splitted[8], splitted[9]);
+
+            }
+
+            sr.Close();
+
+            int ElitJunior = Versenyzo.versenyzok.FindAll(x => x.Kategoria == "elitjunior").Count;
+            Console.WriteLine($"A versenyzők száma elit junior kategóriában: {ElitJunior}");
+        }
+    }
+}
---
+++ Dolgozat/Versenyzo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dolgozat
+{
+
+    internal class Versenyzo
+    {
+        public static List<Versenyzo> versenyzok = new List<Versenyzo>();
+
+        public string Nev { get; set; }
+        public DateTime SzulIdo { get; set; }
+        public int Rajt { get; set; }
+        public char Neme { get; set; }
+        public int Kor { get; set; }
+        public string Kategoria { get; set; }
+
+        public DateTime UszasIdo { get; set; }
+        public DateTime BicikliIdo { get; set; }
+        public DateTime MasodikDepo { get; set; }
+        public DateTime FutasIdo { get; set; }
+
+        public Versenyzo(string nev, DateTime szulIdo, int rajt, char neme, int kor, string kategoria, DateTime uszasIdo, DateTime bicikliIdo, DateTime masodikDepo, DateTime futasIdo)
+        {
+            Nev = nev;
+            SzulIdo = szulIdo;
+            Rajt = rajt;
+            Neme = neme;
+            Kor = kor;
+            Kategoria = kategoria;
+            UszasIdo = uszasIdo;
+            BicikliIdo = bicikliIdo;
+            MasodikDepo = masodikDepo;
+            FutasIdo = futasIdo;
+            UszasIdo = uszasIdo;
+            BicikliIdo = bicikliIdo;
+            MasodikDepo = masodikDepo;
+            FutasIdo = futasIdo;
+
+            versenyzok.Add(this);
+        }
+    }
+        public override string ToString()
+        {
+            return $"{Nev} ({Kor.ToString("yyyy")}) Idő: {Ido} | Kat.:{Kategoria} | Rajtsz.: {Rajt}";
+        }
+    }
+}
